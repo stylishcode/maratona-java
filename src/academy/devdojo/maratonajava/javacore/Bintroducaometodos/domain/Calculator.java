a316f674b2a9cbbfe57039f15760986a66a00466
Matheus Henrique <matheus.hpp135@gmail.com>
2022-02-20 03:03:30 -0300

    45 - métodos - parâmetros

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Calculator.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Calculator.java
@@ -1,11 +1,15 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain;
 
 public class Calculator {
-   public void sum() {
-       System.out.println(10 + 10);
-   }
+    public void sum() {
+        System.out.println(10 + 10);
+    }
 
-   public void subtract() {
-       System.out.println(21-2);
-   }
+    public void subtract() {
+        System.out.println(21 - 2);
+    }
+
+    public void multiply(int num, int num2) {
+        System.out.println(num * num2);
+    }
 }
