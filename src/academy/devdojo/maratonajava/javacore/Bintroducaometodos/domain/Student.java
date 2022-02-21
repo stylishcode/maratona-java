c8fd0a5ac9e138aaab4be6a505c5f9efe1a3df79
Matheus Henrique <matheus.hpp135@gmail.com>
2022-02-21 14:18:18 -0300

    51 - métodos - referência this

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Student.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/Student.java
@@ -4,4 +4,11 @@ public class Student {
     public String name;
     public int age;
     public char gender;
+
+    public void showData() {
+        System.out.println("------------------");
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.gender);
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/StudentTest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.Student;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.StudentData;
+
+public class StudentTest02 {
+    public static void main(String[] args) {
+        Student student = new Student();
+        Student student2 = new Student();
+
+        student.name = "Midoriya";
+        student.age = 15;
+        student.gender = 'M';
+
+        student2.name = "Sakura";
+        student2.age = 16;
+        student2.gender = 'F';
+
+        student.showData();
+        student2.showData();
+    }
+}
