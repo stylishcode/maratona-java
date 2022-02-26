4109189b5fa78aa18526171c24034c58f0f60f42
Matheus Henrique <matheus.hpp135@gmail.com>
2022-02-26 03:21:04 -0300

    77 - modificador final - tipo primitivo

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/domain/Car.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.domain;
+
+public class Car {
+    private String nome;
+    public static final double MAX_SPEED = 250;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
