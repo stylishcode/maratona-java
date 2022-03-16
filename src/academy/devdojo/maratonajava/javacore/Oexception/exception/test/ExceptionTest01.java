b94e8b44d1ec5f1ac578c2554eba4b976f454891
Matheus Henrique <matheus.hpp135@gmail.com>
2022-03-16 20:17:45 -0300

    99 - Exceções - Lançando exceções checked

--- src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest01.java
@@ -4,17 +4,19 @@ import java.io.File;
 import java.io.IOException;
 
 public class ExceptionTest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         createFile();
     }
 
-    public static void createFile() {
+    public static void createFile() throws IOException {
         File file = new File("file2\\test.txt");
         try {
             boolean isCreated = file.createNewFile();
             System.out.println("Created file? " + isCreated);
         } catch (IOException e) {
             e.printStackTrace();
+            // Relançando a exceção
+            throw e;
         }
     }
 }
