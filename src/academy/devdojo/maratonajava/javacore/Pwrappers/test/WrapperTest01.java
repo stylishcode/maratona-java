a78c418b7a6425c227f88d1a450ae5193e0091a9
Matheus Henrique <matheus.hpp135@gmail.com>
2022-03-17 21:23:02 -0300

    107 - Classes Utilitárias - Wrappers pt 02

--- src/academy/devdojo/maratonajava/javacore/Pwrappers/test/WrapperTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Pwrappers/test/WrapperTest01.java
@@ -3,10 +3,10 @@ package academy.devdojo.maratonajava.javacore.Pwrappers.test;
 public class WrapperTest01 {
     public static void main(String[] args) {
         /*
-        * Tipos primitivos. Aqui estamos trabalho com bits, ou seja, o tamanho que cada tipo aloca
-        * É possível alocar um int dentro de um long porque um int cabe em um long, então faz a
-        * conversão implicita
-        * */
+         * Tipos primitivos. Aqui estamos trabalho com bits, ou seja, o tamanho que cada tipo aloca
+         * É possível alocar um int dentro de um long porque um int cabe em um long, então faz a
+         * conversão implicita
+         * */
         byte byteP = 1;
         short shortP = 1;
         int intP = 1;
@@ -17,10 +17,13 @@ public class WrapperTest01 {
         boolean booleanP = false;
 
         /*
-        * Tipos Wrappers. Agora não estamos trabalhando mais com bits e sim com objetos
-        * Não é possível alocar um int dentro de um long porque é aplicado polimorfismo
-        * e na linha de herança, um int não é um long
-        * */
+         * Tipos Wrappers. Agora não estamos trabalhando mais com bits e sim com objetos
+         * Não é possível alocar um int dentro de um long porque é aplicado polimorfismo
+         * e na linha de herança, um int não é um long
+         *
+         * O processo em que o java transforma um tipo primitivo para um Wrapper é chamado de autoboxing
+         * É o que está sendo feito abaixo
+         * */
         Byte byteW = 1;
         Short shortW = 1;
         Integer intW = 1;
@@ -29,5 +32,26 @@ public class WrapperTest01 {
         Double doubleW = 10D;
         Character charW = 'W';
         Boolean booleanW = false;
+        /*
+         * O processo em que o java transforma um Wrapper em um tipo primitivo é chamado de unboxing
+         * */
+        int i = intW; // unboxing
+        Integer intW2 = Integer.parseInt("1");
+        Integer intW3 = new Integer("2"); // deprecated no java 9, não faça isso
+        /*
+         * Todos os Wrappers aceitam parse e uma string como entrada, com exceção do Character
+         * */
+        Boolean isTrue = Boolean.parseBoolean("tRuE");
+        System.out.println(isTrue);
+        /*
+        * Verificações que podem ser feitas usando Character
+        * */
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
     }
 }
