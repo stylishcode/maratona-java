5c9b49274f628061870be2f7178247619a3ec043
Matheus Henrique <matheus.hpp135@gmail.com>
2022-03-30 10:17:36 -0300

    151 - Classes Utilitárias - NIO pt 08 - DosFileAttribute

---
+++ src/academy/devdojo/maratonajava/javacore/Wnio/test/DosFileAttributeTest01.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.DosFileAttributes;
+
+public class DosFileAttributeTest01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Path.of("folder/test.txt");
+        if (Files.notExists(path)) {
+            Files.createFile(path);
+        }
+//      Alterando atributos sem DosFileAttribute
+//      Files.setAttribute(path, "dos:hidden", true);
+//      Files.setAttribute(path, "dos:readonly", true);
+
+//      Usando DosFileAttributes
+        DosFileAttributes dosFileAttributes = Files.readAttributes(path, DosFileAttributes.class);
+        System.out.println("IsHidden? " + dosFileAttributes.isHidden());
+        System.out.println("IsReadonly? " + dosFileAttributes.isReadOnly());
+
+
+//      Alterando atributos com DosFileAttributes
+        DosFileAttributeView fileAttributeView = Files.getFileAttributeView(path, DosFileAttributeView.class);
+        fileAttributeView.setHidden(true);
+        fileAttributeView.setReadOnly(true);
+        System.out.println("IsHidden? " + fileAttributeView.readAttributes().isHidden());
+        System.out.println("IsReadonly? " + fileAttributeView.readAttributes().isReadOnly());
+    }
+}
