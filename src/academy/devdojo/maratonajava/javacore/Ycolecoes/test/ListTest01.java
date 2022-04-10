adbfea69aafbc5eff786c4585520dad57091d0fc
Matheus Henrique <matheus.hpp135@gmail.com>
2022-04-10 15:21:14 -0300

    167 - Coleções pt 07 - List pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -23,13 +23,38 @@ public class ListTest01 {
 //        names.add(121); // Agora não funciona, pois o tipo é int
         for (String name : names) {
             System.out.println(name);
+//            names.add("Matheus"); // não pode, lança ConcurrentModificationException
         }
         names.add("Suane");
         System.out.println("-----------------");
         // Percorrendo lista com for indexado
         for (int i = 0; i < names.size(); i++) {
             System.out.println(names.get(i));
+//            names.add("Matheus"); // pode, porém entra em loop infinito porque o size da lista sempre vai aumentar em +1
         }
 
+        // Removendo por indice
+        System.out.println(names.remove(0)); // William
+
+        // Removendo por Objeto
+        names.remove("Suane"); // true se a lista conter o elemento especificado
+        System.out.println("------ após remoção ------");
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        // Criando uma nova lista e adicionando ela a lista name
+        List<String> names2 = new ArrayList<>();
+        names2.add("Matheus");
+        names2.add("Henrique");
+        names.addAll(names2);
+
+        System.out.println("------Após adicionar names2 a names------");
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
     }
 }
