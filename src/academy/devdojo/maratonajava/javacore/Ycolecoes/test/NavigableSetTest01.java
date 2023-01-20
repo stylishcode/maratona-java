912aae213300f60267c1b8a85a44f16e29f6ed66
Matheus Henrique <matheus.hpp135@gmail.com>
2023-01-20 13:52:41 -0300

    177 - Coleções pt 17 - NavigableSet, TreeSet

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
@@ -13,22 +13,49 @@ class SmartphoneBrandComparator implements Comparator<Smartphone> {
         return o1.getBrand().compareTo(o2.getBrand());
     }
 }
+
+class MangaPriceComparator implements Comparator<Manga> {
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPrice(), o2.getPrice());
+    }
+}
+
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneBrandComparator());
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
 
-        NavigableSet<Manga> mangas = new TreeSet<>();
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPriceComparator());
         mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9, 0));
         mangas.add(new Manga(1L, "Berserk", 9.5, 5));
         mangas.add(new Manga(4L, "Pokemon", 3.2, 0));
         mangas.add(new Manga(3L, "Attack on titan", 11.20, 2));
         mangas.add(new Manga(2L, "Dragon ball Z", 2.99, 0));
         mangas.add(new Manga(10L, "Aaragon", 2.99, 0));
-        for (Manga manga : mangas) {
+//        for (Manga manga : mangas) {
+//            System.out.println(manga);
+//        }
+        // inverte a ordem de exibição do TreeSet
+        for (Manga manga : mangas.descendingSet()) {
             System.out.println(manga);
         }
 
+        System.out.println("----------------");
+
+        Manga yuyu = new Manga(19L, "Tokyo Revengers", 19.9, 5);
+
+        // mangas.lower(yuyu) -> retorna o < preço depois de yuyu
+        // mangas.floor(yuyu) -> retorna o <= preço depois de yuyu
+        // mangas.higher(yuyu) -> retorna o > preço depois de yuyu
+        // mangas.ceiling(yuyu) -> retorna o >= preço depois de yuyu
+
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst()); // retorna e remove o primeiro elemento do TreeSet
+        System.out.println(mangas.pollLast()); // retorna e remove o ultimo elemento do TreeSet
+        System.out.println(mangas.size());
     }
 }
