d7251661aa9785978ebb068a31768d5c49ad5a75
Matheus Henrique <matheus.hpp135@gmail.com>
2023-01-24 12:46:13 -0300

    182 - Coleções pt 22 - Queue, PriorityQueue

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/QueueTest02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Manga;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueTest02 {
+    public static void main(String[] args) {
+        Queue<Manga> mangas = new PriorityQueue<>(new MangaPriceComparator().reversed());
+        mangas.add(new Manga(5L, "Hellsing ultimate", 19.9));
+        mangas.add(new Manga(1L, "Berseker", 9.5));
+        mangas.add(new Manga(4L, "Pokemon", 3.2));
+        mangas.add(new Manga(3L, "Attack on titan", 11.20));
+        mangas.add(new Manga(2L, "Dragon Ball Z", 2.99));
+
+        while(!mangas.isEmpty()) {
+            System.out.println(mangas.poll());
+        }
+    }
+}
