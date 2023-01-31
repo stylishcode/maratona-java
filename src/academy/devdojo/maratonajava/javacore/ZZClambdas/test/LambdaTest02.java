fa267eaeb722e40ac9c25c021cc2b90ac68d80da
Matheus Henrique <matheus.hpp135@gmail.com>
2023-01-31 11:32:35 -0300

    196 - Lambdas pt 02 - Consumer

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/LambdaTest02.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class LambdaTest02 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("William", "Suane", "Luffy", "Zoro");
+        List<Integer> integers = List.of(1, 2, 3, 4, 5);
+        // apenas imprime a lista passada como argumento
+        forEach(strings, s -> System.out.println(s));
+        forEach(integers, i -> System.out.println(i));
+    }
+
+    /*
+     * Consumer é uma interface funcional, só que diferente da Predicate que retorna um booleano, Consumer
+     * não retorna nada
+     * */
+    private static <T> void forEach(List<T> list, Consumer<T> consumer) {
+        for (T e : list) {
+            consumer.accept(e);
+        }
+    }
+}
