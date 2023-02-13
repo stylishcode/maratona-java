55ce5c9503f87f9be8540837de7c1150c6c44a6f
Matheus Henrique <matheus.hpp135@gmail.com>
2023-02-13 16:34:12 -0300

    225 - Threads pt 06 - Sincronismo de thread pt 02

--- src/academy/devdojo/maratonajava/javacore/ZZFthreads/ThreadAccountTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZFthreads/ThreadAccountTest01.java
@@ -5,7 +5,7 @@ import academy.devdojo.maratonajava.javacore.ZZFthreads.domain.Account;
 import java.util.function.Supplier;
 
 public class ThreadAccountTest01 implements Runnable {
-    private Account account = new Account();
+    private final Account account = new Account();
 
     public static void main(String[] args) {
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
@@ -26,13 +26,17 @@ public class ThreadAccountTest01 implements Runnable {
     }
 
     private void withdrawal(int amount) {
-       if (account.getBalance() >= amount) {
-           System.out.println(getThreadName() + " está indo sacar dinheiro");
-           account.withdrawal(amount);
-           System.out.println(getThreadName() + " completou o saque, valor atual da conta " + account.getBalance());
-       } else {
-           System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque " + account.getBalance());
-       }
+        System.out.println(getThreadName() + " #### fora do synchronized block");
+        synchronized (account) {
+            System.out.println(getThreadName() + " **** dentro do synchronized block");
+            if (account.getBalance() >= amount) {
+                System.out.println(getThreadName() + " está indo sacar dinheiro");
+                account.withdrawal(amount);
+                System.out.println(getThreadName() + " completou o saque, valor atual da conta " + account.getBalance());
+            } else {
+                System.out.println("Sem dinheiro para " + getThreadName() + " efetuar o saque " + account.getBalance());
+            }
+        }
     }
 
     private String getThreadName() {
