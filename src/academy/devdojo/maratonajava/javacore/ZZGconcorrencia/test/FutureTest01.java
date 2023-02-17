f51bd891da79c7db3a8de4eb98a86397356d2acb
Matheus Henrique <matheus.hpp135@gmail.com>
2023-02-17 11:30:27 -0300

    239 - Concorrência pt 11 - Future

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/FutureTest01.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.*;
+
+public class FutureTest01 {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Double> dollarRequest = executorService.submit(() -> {
+            TimeUnit.SECONDS.sleep(7);
+            return 4.35D;
+        });
+        System.out.println(doSomething());
+        Double dollarResponse = null;
+        try {
+            dollarResponse = dollarRequest.get(3, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
+            e.printStackTrace();
+        } finally {
+            executorService.shutdown();
+        }
+        System.out.printf("Cotaçao do dólar: %f%n", dollarResponse);
+    }
+
+    private static Long doSomething() {
+        System.out.println(Thread.currentThread().getName());
+        long sum = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
