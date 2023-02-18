a7fc268048dc19a55194914b4d9d4a4e3bbae2e4
Matheus Henrique <matheus.hpp135@gmail.com>
2023-02-18 04:32:45 -0300

    243 - Concorrência pt 15-16 - CompletableFuture pt 04-05 - Encadeando chamadas pt 01-02

---
+++ src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/service/StoreServiceWithDiscount.java
@@ -0,0 +1,42 @@
+package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.service;
+
+import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.domain.Discount;
+import academy.devdojo.maratonajava.javacore.ZZGconcorrencia.domain.Quote;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class StoreServiceWithDiscount {
+
+    // precisamos retornar nesse padrão: storeName:price:discountCode
+    public String getPriceSync(String storeName) {
+        // poderia vir de uma chamada a um servidor remoto por ex
+        double price = priceGenerator();
+        // pega um valor aleatório da enumeração (que poderia vir do banco de dados por ex)
+        Discount.Code discountCode = Discount.Code
+                .values()[ThreadLocalRandom.current().nextInt(Discount.Code.values().length)];
+        return String.format("%s:%.2f:%s", storeName, price, discountCode);
+    }
+
+    private double priceGenerator() {
+        delay();
+        return ThreadLocalRandom.current().nextInt(1, 500) * 10;
+    }
+
+    public String applyDiscount(Quote quote) {
+        // simulando que para aplicar um desconto, é necessário fazer outra chamada em outro serviço
+        delay();
+        // Como pode gerar um desconto 0. Fazemos essa formula para que retorne 1 quando não tiver desconto
+        double discountValue = quote.getPrice() * (100 - quote.getDiscountCode().getPercentage()) / 100;
+        return String.format("'%s' original price: '%.2f'. Applying discount code '%s'. Final price: '%.2f'",
+                quote.getStore(), quote.getPrice(), quote.getDiscountCode(), discountValue);
+    }
+
+    private void delay() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
