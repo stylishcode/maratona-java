7a6b51a170958257a912f733f745ad989994190f
Matheus Henrique <matheus_hpp135@outlook.com>
2023-02-19 05:56:52 -0300

    250 - Padrões de Projeto pt 05 - Singleton pt 03 - Singleton with enumeration

---
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/test/AircraftSingletonEnumTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.test;
+
+import academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.domain.AircraftSingletonEnum;
+
+public class AircraftSingletonEnumTest01 {
+    public static void main(String[] args) {
+        bookSeat("1A");
+        bookSeat("1A");
+    }
+
+    private static void bookSeat(String seat) {
+        // como agora não tem como ver o espaço em memória criado, usamos o hashCode para ver se são iguais
+        System.out.println(AircraftSingletonEnum.INSTANCE.hashCode());
+        AircraftSingletonEnum instance = AircraftSingletonEnum.INSTANCE;
+        System.out.println(instance.bookSeat(seat));
+    }
+}
